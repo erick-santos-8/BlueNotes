5057c5ca3be28a41cb5b193edb9558693611c450
erick-santos-8 <esagosto@hotmail.com>
2024-11-21 16:38:14 -0300

    Modal to edit and create notes done

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,42 +1,62 @@
 import { BsPlus } from 'react-icons/bs'
 import NoteCard from '../../components/cards/NoteCard'
+import EditAndCreateNote from './modal/EditAndCreateNote'
+import { useState } from 'react';
+import Modal from "react-modal";
 
 const Home = () => {
+
+  const [openModal, setOpenModal] = useState({
+    isOpen: false,
+    type: "add",
+    date: null
+  });
+
   return (
     <>
       <div className='container mx-auto m-2 '>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6 drop-shadow-md'>
           <NoteCard title="Estudar React" date="19/11/2024" content="Estudar React, JavaScript e Node.js" tags={["React", "JavaScript", "Node.js"]} isPinned={true}
-            onPin={() => {console.log("pin")}}
-            onEdit={() => {console.log("edit")}}
-            onDelete={() => {console.log("delete")}}
+            onPin={() => { console.log("pin") }}
+            onEdit={() => { console.log("edit") }}
+            onDelete={() => { console.log("delete") }}
           />
           <NoteCard title="Estudar React" date="19/11/2024" content="Estudar React, JavaScript e Node.js" tags={["React", "JavaScript", "Node.js"]} isPinned={true}
-            onPin={() => {console.log("pin")}}
-            onEdit={() => {console.log("edit")}}
-            onDelete={() => {console.log("delete")}}
+            onPin={() => { console.log("pin") }}
+            onEdit={() => { console.log("edit") }}
+            onDelete={() => { console.log("delete") }}
           />
           <NoteCard title="Estudar React" date="19/11/2024" content="Estudar React, JavaScript e Node.js" tags={["React", "JavaScript", "Node.js"]} isPinned={true}
-            onPin={() => {console.log("pin")}}
-            onEdit={() => {console.log("edit")}}
-            onDelete={() => {console.log("delete")}}
+            onPin={() => { console.log("pin") }}
+            onEdit={() => { console.log("edit") }}
+            onDelete={() => { console.log("delete") }}
           />
           <NoteCard title="Estudar React" date="19/11/2024" content="Estudar React, JavaScript e Node.js" tags={["React", "JavaScript", "Node.js"]} isPinned={true}
-            onPin={() => {console.log("pin")}}
-            onEdit={() => {console.log("edit")}}
-            onDelete={() => {console.log("delete")}}
+            onPin={() => { console.log("pin") }}
+            onEdit={() => { console.log("edit") }}
+            onDelete={() => { console.log("delete") }}
           />
           <NoteCard title="Estudar React" date="19/11/2024" content="Estudar React, JavaScript e Node.js" tags={["React", "JavaScript", "Node.js"]} isPinned={true}
-            onPin={() => {console.log("pin")}}
-            onEdit={() => {console.log("edit")}}
-            onDelete={() => {console.log("delete")}}
+            onPin={() => { console.log("pin") }}
+            onEdit={() => { console.log("edit") }}
+            onDelete={() => { console.log("delete") }}
           />
         </div>
       </div>
 
-      <button onClick={() => {console.log("click")}} className="bg-blue-700 dark:bg-blue-200 dark:text-blue-950 text-white rounded-full px-4 py-4 text-sm hover:bg-blue-500 dark:hover:bg-blue-400 transition-all duration-200 ease-in-out fixed bottom-5 right-5">
-        <BsPlus className='text-2xl' />
+      <button onClick={() => {
+          setOpenModal({
+            isOpen: true,
+            type: "add",
+            date: null
+          })     }} 
+        className="bg-blue-700 dark:bg-blue-200 dark:text-blue-950 text-white rounded-full px-4 py-4 text-sm hover:bg-blue-500 dark:hover:bg-blue-400 transition-all duration-200 ease-in-out fixed bottom-5 right-5">
+          <BsPlus className='text-2xl' />
       </button>
+
+      <Modal isOpen={openModal.isOpen} onRequestClose={() => { }} contentLabel="" className=" w-full md:w-[50rem] max-h-3/4 rounded-md mx-auto p-5 overflow-scroll bg-transparent">
+        <EditAndCreateNote />
+      </Modal>
     </>
   )
 }
---
+++ frontend/src/pages/home/modal/EditAndCreateNote.jsx
@@ -0,0 +1,29 @@
+
+const EditAndCreateNote = () => {
+  return (
+    <div className='flex flex-col justify-center items-center gap-2 p-4 bg-gray-100 dark:bg-gray-900 rounded-lg drop-shadow-lg shadow-md'>
+      <div className='flex flex-col justify-center items-start gap-2 w-full'>
+        <label className='text-lg font-bold text-gray-800 dark:text-gray-200'>Título</label>
+        <input type="text" id="title" className='input-box w-full text-xl outline-none' placeholder='Estudar React' />
+      </div>
+
+      <div className='flex flex-col justify-center items-start gap-2 w-full'>
+        <label className='text-lg font-bold text-gray-800 dark:text-gray-200'>Conteudo</label>
+        <textarea id="content" className='input-box w-full text-xs outline-none' rows={10} placeholder='Estudar React'></textarea>
+      </div>
+
+      <div className='flex flex-col justify-center items-start gap-2 w-full'>
+        <label className='text-lg font-bold text-gray-800 dark:text-gray-200'>Tags</label>
+        <input type="text" id="tags" className='input-box w-full text-xs outline-none' placeholder='React JavaScript Node.js' />
+      </div>
+
+      <button className='button-input-box'>Criar</button>
+
+    </div>
+
+    
+      
+  )
+}
+
+export default EditAndCreateNote
